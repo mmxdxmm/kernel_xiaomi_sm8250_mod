3521bb2d6e51b7b07fe9d4cc019edb3ead346d76
mmxdxmm <145817075+mmxdxmm@users.noreply.github.com>
2025-03-11 15:41:31 +0800

    Add files via upload

--- build_thinlto.sh
+++ build_thinlto.sh
@@ -4,9 +4,12 @@
 
 # Ensure the script exits on error
 set -e
-yes | tar -xvf electron-binutils-2.41.tar.xz
+
+wget https://github.com/kdrag0n/proton-clang/archive/refs/tags/20210522.zip
+yes | unzip 20210522.zip
+#yes | tar -xvf electron-binutils-2.41.tar.xz
 TOOLCHAIN_PATH=/lib/llvm-20/bin
-BINUTILS_PATH=$PWD/electron-binutils-2.41/bin
+BINUTILS_PATH=$PWD/proton-clang-20210522/bin
 GIT_COMMIT_ID="mmxdxmm"
 
 TARGET_DEVICE=$1
