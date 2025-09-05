09807e8ce473a8ac0f51d5d4d2854b5411f38862
Yanfeng Lee <liyafe1997@126.com>
2025-09-05 11:07:11 +0200

    Change SukiSU source (since upstream has deleted old versions)

--- build.sh
+++ build.sh
@@ -95,7 +95,7 @@ echo "TARGET_DEVICE: $TARGET_DEVICE"
 
 if [ $KSU_ENABLE -eq 1 ]; then
     echo "KSU is enabled"
-    curl -LSs "https://raw.githubusercontent.com/ShirkNeko/SukiSU-Ultra/main/kernel/setup.sh" | bash -s f4863b20cc8dc0f8cc67418980f022e43014b598
+    curl -LSs "https://github.com/liyafe1997/SukiSU-Ultra/raw/4ff14cf0051d04209c4abd5027d99d8e7780ef5b/kernel/setup.sh" | bash -s f4863b20cc8dc0f8cc67418980f022e43014b598
 else
     echo "KSU is disabled"
 fi
